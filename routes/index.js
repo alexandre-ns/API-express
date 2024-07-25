5314d3e872f7211c13a8e2fb2f5d7c744eb839ac
alexandre <alexandren49@gmail.com>
2024-07-25 14:22:15 -0300

    add routes order and routes cart in routes index file

--- routes/index.js
+++ routes/index.js
@@ -4,10 +4,14 @@ var router = express.Router();
 const routesProducts = require('./products');
 const routesUser = require('./users');
 const routesCategory = require('./category');
+const routesOrder = require('./order');
+const routesCart = require('./cart');
 
 router.use('/products', routesProducts);
 router.use('/users', routesUser);
 router.use('/category', routesCategory);
+router.use('/category', routesOrder);
+router.use('/routesCart', routesCategory);
 
 // GET home page. 
 router.get('/', function(req, res, next) {
