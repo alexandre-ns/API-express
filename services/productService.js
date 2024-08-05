741235ae91f408e43fe0e4f2e548b43b53e414c4
alexandre <alexandren49@gmail.com>
2024-08-05 17:33:02 -0300

    fix bug field

--- services/productService.js
+++ services/productService.js
@@ -1,56 +1,64 @@
-const ProductRepository = require("../repositories/productRepository");
+const productRepository = require("../repositories/productRepository");
 
-class UserService {
+class ProductService {
   async getAllProducts() {
-    const products = await ProductRepository.findAll();
+    const products = await productRepository.findAll();
     return products.map((product) => ({
       id: product._id,
       name: product.name,
-      price: product.email,
+      price: product.price,
       category: product.category,
       quantityStock: product.quantityStock,
+      description: product.description,
+      createdAt: product.createdAt
     }));
   }
 
   async getProductById(id) {
-    const product = await ProductRepository.findById(id);
+    const product = await productRepository.findById(id);
     return product
       ? {
           id: product._id,
           name: product.name,
-          price: product.email,
+          price: product.price,
           category: product.category,
           quantityStock: product.quantityStock,
+          description: product.description,
+          createdAt: product.createdAt
         }
       : null;
   }
 
   async createProduct(product) {
-    const newProduct = await ProductRepository.create(product);
+    const newProduct = await productRepository.create(product);
     return {
       id: newProduct._id,
       name: newProduct.name,
-      price: newProduct.email,
+      price: newProduct.price,
       category: newProduct.category,
       quantityStock: newProduct.quantityStock,
+      description: product.description,
+      createdAt: product.createdAt
     };
   }
 
   async updateProduct(id, product) {
-    const updatedProduct = await ProductRepository.update(id, product);
+    const updatedProduct = await productRepository.update(id, product);
     return updatedProduct
       ? {
           id: updatedProduct._id,
           name: updatedProduct.name,
-          price: updatedProduct.email,
+          price: updatedProduct.price,
           category: updatedProduct.category,
           quantityStock: updatedProduct.quantityStock,
+          description: product.description,
+          createdAt: product.createdAt
         }
       : null;
   }
 
   async deleteProduct(id) {
-    const deletedProduct = await ProductRepository.delete(id);
+    const deletedProduct = await productRepository.delete(id);
     return deletedProduct
       ? {
           id: deletedProduct._id,
@@ -58,12 +66,14 @@ class UserService {
           price: deletedProduct.email,
           category: deletedProduct.category,
           quantityStock: deletedProduct.quantityStock,
+          description: product.description,
+          createdAt: product.createdAt,
         }
       : null;
   }
 }
 
-module.exports = new UserService();
+module.exports = new ProductService();
 
 /* services/userService.js
 const category = require("../models/category");
