37a8e73faef7f8112e2aeabf3f467bfe8616afa8
alexandre <alexandren49@gmail.com>
2024-08-07 17:27:02 -0300

    comments

--- app.js
+++ app.js
@@ -6,9 +6,7 @@ var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 const connectDB = require('./mongoose');
-const apiRoutes = require('./routes'); // ROUTES
-
-
+const apiRoutes = require('./routes');
 
 var app = express();
 app.use(bodyParser.json());
--- middlewares/validator/validatorProduct.js
+++ middlewares/validator/validatorProduct.js
@@ -5,12 +5,12 @@ const validateProduct = (req, res, next) => {
   const errors = validationResult(req);
   if (!errors.isEmpty()) {
     var validatorInfos = errors.array()[0].msg;
-    if (validatorInfos.code == "ID_REQUIRED") {
+    if (validatorInfos.code == "ID_REQUIRED") {// code as id 
       res.status(400).json({ message: validatorInfos.msg.message });
     } else if (validatorInfos.code == "ID_INVALID") {
       res.status(404).json({ message: validatorInfos.message });
     } else {
-      next(errors); //passa para o middleware de tratamento de erros global
+      next(errors);
     }
   }
    
