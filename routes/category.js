55acf4d67698c711d250e14f631056ec33cf7f9b
alexandre <alexandren49@gmail.com>
2024-08-07 17:28:36 -0300

    comments

--- routes/category.js
+++ routes/category.js
@@ -2,11 +2,10 @@ const express = require('express');
 const router = express.Router();
 const categoryController = require('../controllers/categoryController')
 
-// Definindo rotas e associando controladores
 router.get('/', categoryController.getAllCategories);
 router.get('/:id', categoryController.getCategoryById);
 router.post('/', categoryController.createCategory);
 router.put('/:id', categoryController.updateCategory);
 router.delete('/:id', categoryController.deleteCategory);
 
-module.exports = router; // Exporta o roteador
+module.exports = router;
