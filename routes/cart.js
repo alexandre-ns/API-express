cb66223dc94bb9bf707334ed2bcb5448f38dc09c
alexandre <alexandren49@gmail.com>
2024-08-09 19:20:52 -0300

    cart validation

--- routes/cart.js
+++ routes/cart.js
@@ -1,11 +1,43 @@
 const express = require('express');
 const router = express.Router();
-const CartController = require('../controllers/cartController')
+const cartController = require('../controllers/cartController')
+const generalMiddleware = require("../middlewares/general/generalMiddleware");
+const {
+  cartValidationID,
+  cartValidationAll,
+  cartValidationSome
+} = require("../validation/cartValidation");
+const { checkSchema } = require("express-validator");
 
-router.get('/', CartController.getAllCarts);
-router.get('/:id', CartController.getCartById);
-router.post('/', CartController.createCart);
-router.put('/:id', CartController.updateCart);
-router.delete('/:id', CartController.deleteCart);
+
+router.get("/", cartController.getAllCarts);
+
+router.get(
+  "/:id",
+  checkSchema(cartValidationID),
+  generalMiddleware,
+  cartController.getCartById
+);
+
+router.post(
+  "/",
+  checkSchema(cartValidationAll),
+  generalMiddleware,
+  cartController.createCart
+);
+
+router.put(
+  "/:id",
+  checkSchema(cartValidationSome),
+  generalMiddleware,
+  cartController.updateCart
+);
+
+router.delete(
+  "/:id",
+  checkSchema(cartValidationID),
+  generalMiddleware,
+  cartController.deleteCart
+);
 
 module.exports = router;
---
+++ validation/cartValidation.js
@@ -0,0 +1,78 @@
+const cartValidationID = {
+  id: {
+    in: ["params"],
+    isMongoId: {
+      errorMessage: "Invalid Cart ID",
+    },
+    notEmpty: {
+      errorMessage: "Cart ID is required",
+    },
+  },
+};
+
+const cartValidationAll = {
+  "products.*.product": {
+    in: ["body"],
+    isMongoId: {
+      errorMessage: "Invalid product ID",
+    },
+    notEmpty: {
+      errorMessage: "Product ID is required",
+    },
+    optional: false,
+  },
+  "products.*.quantity": {
+    in: ["body"],
+    isInt: {
+      errorMessage: "Quantity must be greater than 0",
+      options: { min: 1 },
+    },
+    notEmpty: {
+      errorMessage: "Quantity is required",
+    },
+    optional: false,
+  },
+  user: {
+    isMongoId: {
+      errorMessage: "Invalid User ID",
+    },
+    notEmpty: {
+      errorMessage: "User ID is required",
+    },
+  },
+};
+
+const cartValidationSome = {
+  "products.*.product": {
+    in: ["body"],
+    isMongoId: {
+      errorMessage: "Invalid product ID",
+    },
+    notEmpty: {
+      errorMessage: "Product ID is required",
+    }
+  },
+  "products.*.quantity": {
+    in: ["body"],
+    isInt: {
+      errorMessage: "Quantity must be greater than 0",
+      options: { min: 1 },
+    },
+    notEmpty: {
+      errorMessage: "Quantity is required",
+    },
+  },
+  user: {
+    isMongoId: {
+      errorMessage: "Invalid User ID",
+    },
+    notEmpty: {
+      errorMessage: "User ID is required",
+    },
+  },
+};
+module.exports = {
+  cartValidationID,
+  cartValidationAll,
+  cartValidationSome
+};
