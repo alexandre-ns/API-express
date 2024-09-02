658c1c89ee2b7c9e52e910b74ae022394aa2086b
alexandre <alexandren49@gmail.com>
2024-09-02 18:44:13 -0300

    feat: routes user auth

--- src/routes/index.js
+++ src/routes/index.js
@@ -6,11 +6,13 @@ const routesUser = require('./users');
 const routesCategory = require('./category');
 const routesOrder = require('./order');
 const routesCart = require('./cart');
+const routesAuth = require('./auth');
 
 router.use('/products', routesProducts);
 router.use('/users', routesUser);
 router.use('/category', routesCategory);
 router.use('/order', routesOrder);
 router.use('/cart', routesCart);
+router.use('/auth', routesAuth);
 
 module.exports = router;
