e691a4d3f84bbd47deaf76cfdeda06319b6d4483
alexandre <alexandren49@gmail.com>
2025-03-18 00:04:12 -0300

    docs: delete log token

--- src/middlewares/auth/adminAuth.js
+++ src/middlewares/auth/adminAuth.js
@@ -1,8 +1,7 @@
 const API_ADMIN_TOKEN = process.env.API_ADMIN_TOKEN;
 
 const adminAuthMiddleware = (req, res, next) => {
-  //console.log(ADMIN_TOKEN);
-  console.log('authorization', req.headers);
+
   const token = req.header('token');
   if (!token) {
     return res.status(401).json({ message: 'Access denied.' });
