7c8529c004605658a7633343bc14d11f54cede6d
alexandre <alexandren49@gmail.com>
2025-03-18 00:02:01 -0300

    build: queue rabbitmq container

--- src/config/rabbitmq/rabbitmqServer.sh
+++ src/config/rabbitmq/rabbitmqServer.sh
@@ -2,11 +2,15 @@
 #for Debian/Ubuntu:
 
 #install rabbitmq server
-sudo apt-get update
-sudo apt-get install rabbitmq-server
+#sudo apt-get update
+#sudo apt-get install rabbitmq-server
 
 # check status
-systemctl status rabbitmq-server
+#systemctl status rabbitmq-server
+
+sudo docker pull rabbitmq:3-management
+
+sudo docker run -d --name rabbitmq-container -p 5672:5672 -p 15672:15672 rabbitmq:3-management
 
 
 # start rabbitmq -> start rabbitmq temporarily
